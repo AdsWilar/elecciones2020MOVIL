239a8a8c9cd9e78d94680f092e18d7b152b42ddd
adswilar <wilarads@gmail.com>
2020-11-19 00:28:47 -0400

    first commit

---
+++ lib/global.dart
@@ -0,0 +1,19 @@
+
+//Server IP Address
+const SERVER_NAME = "https://elecc2020sw1.herokuapp.com/api/";
+
+const URL_LOGIN = "${SERVER_NAME}login";
+const URL_VERNUMMES = "${SERVER_NAME}verificarNumeroMesa";
+const URL_PROCTEXTIMAG = "${SERVER_NAME}procesarTextoImagen";
+
+
+
+//Nombre de la app
+const NOMBRE_APP = "TU VOTO CUENTA";
+
+
+int globalIdUsuario;
+String globalUsuario;
+String globalNombres;
+String globalApellidos;
+String globalEmailUsuario;
